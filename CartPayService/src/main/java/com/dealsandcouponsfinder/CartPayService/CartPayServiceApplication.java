1cd392d354d0e0f4625f07e4506cf99c20819bff
jparvath <115786149+jparvath@users.noreply.github.com>
2022-12-19 09:58:53 +0530

    Add files via upload

---
+++ CartPayService/src/main/java/com/dealsandcouponsfinder/CartPayService/CartPayServiceApplication.java
@@ -0,0 +1,28 @@
+package com.dealsandcouponsfinder.CartPayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableEurekaClient
+@SpringBootApplication
+@EnableSwagger2
+public class CartPayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartPayServiceApplication.class, args);
+	}
+ //create a bean for rest template //create a method that returns instance of a rest template
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+
+	}
+
+}
