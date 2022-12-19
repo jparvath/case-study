d3b5e3792272917da40ce90b04aca31fe86ee39e
jparvath <115786149+jparvath@users.noreply.github.com>
2022-12-19 10:19:21 +0530

    Add files via upload

---
+++ Order-Service/Order-Service/src/main/java/com/order/orderservice/model/Order.java
@@ -0,0 +1,32 @@
+package com.order.orderservice.model;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Document(collection="order")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class Order {
+	@Id
+	private int orderId;
+
+	private int userId;
+	private List<Cart> cart;
+	private String orderStatus;
+	private List<Address> address;
+	private double total;
+	public String getOrderStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
